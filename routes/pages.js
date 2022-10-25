9f286e2a21ec6383d826458a80a319cd0d378e6b
NaughtyRavenn <bachminh.uit@gmail.com>
2022-10-25 19:23:23 +0700

    Add admin site

--- routes/pages.js
+++ routes/pages.js
@@ -6,6 +6,10 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+router.get("/admin", (req, res) => {
+  res.render("admin");
+});
+
 router.get("/register", (req, res) => {
   res.render("register");
 });
